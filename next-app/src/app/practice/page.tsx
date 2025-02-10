e647e41f7b8e2f94cba94d863c4a09a1d3da829b
chaeyeongLEE <dh503401@naver.com>
2025-02-10 10:00:24 +0900

    feat: Nav바 추가

--- next-app/src/app/practice/page.tsx
+++ next-app/src/app/practice/page.tsx
@@ -24,106 +24,129 @@ const Practice = () => {
     console.log("subscribe click");
   }
 
+  const [active, setActive] = useState("All");
+  const tabs = ["All", "About", "Projects", "Media"];
+
   return (
-    <div className={styles.dashboard}>
-      <section className={styles.profile}>
-        <p className="">
-          I’m{" "}
-          <span style={{ fontFamily: "Moranga Bold", fontSize: "40px" }}>
-            nev
-          </span>
-          , a developer and product designer from Ireland. I'm interested in
-          React, Node, Product Design, Jamstack, Startups, Cryptocurrencies and
-          Music.
-        </p>
-      </section>
-      <section className={styles.map}>Map Section</section>
-      <section className={styles.star}>
-        <Image
-          alt="starBucks"
-          src={starBucks}
-          className={styles.starImage}
-          style={{ width: "280px" }}
-        />
-      </section>
-      <section className={styles.spotify}>
-        <Image alt="spotify" src={spotify} />
-        <article>
-          <p style={{ color: "rgb(110, 210, 183)", fontWeight: 500 }}>
-            Offline. Last played
-          </p>
-          <h2>I Don't Belong</h2>
-          <p style={{ letterSpacing: "1.2px" }}>Fontaines D.C.</p>
-        </article>
-        {/*<div className={styles.spotifyBox}>*/}
-        {/*  <div className={styles.spotifyMusic}></div>*/}
-        {/*  <div className={styles.spotifyMusic}></div>*/}
-        {/*  <div className={styles.spotifyMusic}></div>*/}
-        {/*</div>*/}
-      </section>
-      <section className={styles.card}>
-        <Image alt="twitter" src={twitterIcon} className={styles.twitterIcon} />
-        <a className={styles.arrow}>
-          <Image alt="" src={arrowIcon} />
-        </a>
-      </section>
-      <section className={styles.article}>
-        <Image alt="starBucks" src={book} style={{ width: "280px" }} />
-      </section>
-      <section className={styles.wideCard}>
-        <div className={styles.textBox}>
-          <h2>How it started vs. how it's going</h2>
-          <p>
-            A short personal history as it relates to design and development,
-            and how I've found value in the cross-section between both
-            disciplines.
-          </p>
-        </div>
-        <div className={styles.box}>
-          <CustomButton
-            title="Read more"
-            type="button"
-            disabled={false}
-            className="subscribeBtn"
-            handleClick={isSubscribeClick}>
-            Subscribe
-          </CustomButton>
-          <p>Feb 7, 2025</p>
-        </div>
-      </section>
-      <section className={styles.darkMode}>
-        <label className={styles.switch}>
-          <input type="checkbox" checked={darkMode} onChange={toggleDarkMode} />
-          <span className={styles.slider}></span>
-        </label>
-      </section>
-      <section className={styles.admin}>
-        <Image alt="admins" src={admins} style={{}} />
-      </section>
-      <section className={styles.subscribe}>
-        <h2>Shall I keep you in the loop?</h2>
-        <p>
-          Content includes articles, early access to products, and <br />
-          ongoing learnings.
-        </p>
-        <input type="text" placeholder="Email address" />
-        <div>
-          <CustomButton
-            title="subscribe"
-            type="button"
-            disabled={false}
-            className="subscribeBtn"
-            handleClick={isSubscribeClick}>
-            Subscribe
-          </CustomButton>
-          <p>
-            You'll be subscriber number{" "}
-            <span style={{ fontFamily: "Moranga Bold", fontSize: "30px" }}>
-              541
+    <div className={styles.container}>
+      <div className={styles.nav}>
+        {tabs.map((tab) => (
+          <div
+            key={tab}
+            className={`${styles["nav-item"]} ${active === tab ? styles.active : ""}`}
+            onClick={() => setActive(tab)}>
+            {tab}
+          </div>
+        ))}
+      </div>
+      <div className={styles.dashboard}>
+        <section className={styles.profile}>
+          <p className="">
+            I’m{" "}
+            <span style={{ fontFamily: "Moranga Bold", fontSize: "40px" }}>
+              nev
             </span>
+            , a developer and product designer from Ireland. I'm interested in
+            React, Node, Product Design, Jamstack, Startups, Cryptocurrencies
+            and Music.
           </p>
-        </div>
-      </section>
+        </section>
+        <section className={styles.map}>Map Section</section>
+        <section className={styles.star}>
+          <Image
+            alt="starBucks"
+            src={starBucks}
+            className={styles.starImage}
+            style={{ width: "280px" }}
+          />
+        </section>
+        <section className={styles.spotify}>
+          <Image alt="spotify" src={spotify} />
+          <article>
+            <p style={{ color: "rgb(110, 210, 183)", fontWeight: 500 }}>
+              Offline. Last played
+            </p>
+            <h2>I Don't Belong</h2>
+            <p style={{ letterSpacing: "1.2px" }}>Fontaines D.C.</p>
+          </article>
+          {/*<div className={styles.spotifyBox}>*/}
+          {/*  <div className={styles.spotifyMusic}></div>*/}
+          {/*  <div className={styles.spotifyMusic}></div>*/}
+          {/*  <div className={styles.spotifyMusic}></div>*/}
+          {/*</div>*/}
+        </section>
+        <section className={styles.card}>
+          <Image
+            alt="twitter"
+            src={twitterIcon}
+            className={styles.twitterIcon}
+          />
+          <a className={styles.arrow}>
+            <Image alt="" src={arrowIcon} />
+          </a>
+        </section>
+        <section className={styles.article}>
+          <Image alt="starBucks" src={book} style={{ width: "280px" }} />
+        </section>
+        <section className={styles.wideCard}>
+          <div className={styles.textBox}>
+            <h2>How it started vs. how it's going</h2>
+            <p>
+              A short personal history as it relates to design and development,
+              and how I've found value in the cross-section between both
+              disciplines.
+            </p>
+          </div>
+          <div className={styles.box}>
+            <CustomButton
+              title="Read more"
+              type="button"
+              disabled={false}
+              className="subscribeBtn"
+              handleClick={isSubscribeClick}>
+              Subscribe
+            </CustomButton>
+            <p>Feb 7, 2025</p>
+          </div>
+        </section>
+        <section className={styles.darkMode}>
+          <label className={styles.switch}>
+            <input
+              type="checkbox"
+              checked={darkMode}
+              onChange={toggleDarkMode}
+            />
+            <span className={styles.slider}></span>
+          </label>
+        </section>
+        <section className={styles.admin}>
+          <Image alt="admins" src={admins} style={{}} />
+        </section>
+        <section className={styles.subscribe}>
+          <h2>Shall I keep you in the loop?</h2>
+          <p>
+            Content includes articles, early access to products, and <br />
+            ongoing learnings.
+          </p>
+          <input type="text" placeholder="Email address" />
+          <div>
+            <CustomButton
+              title="subscribe"
+              type="button"
+              disabled={false}
+              className="subscribeBtn"
+              handleClick={isSubscribeClick}>
+              Subscribe
+            </CustomButton>
+            <p>
+              You'll be subscriber number{" "}
+              <span style={{ fontFamily: "Moranga Bold", fontSize: "30px" }}>
+                541
+              </span>
+            </p>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
